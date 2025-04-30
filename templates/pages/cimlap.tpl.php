206ac9d5605b18d174d99fe0db642c1ba664cc85
Danagen <dancen12@gmail.com>
2025-04-30 06:48:18 +0200

    cimlap.tpl.php video embedd

--- templates/pages/cimlap.tpl.php
+++ templates/pages/cimlap.tpl.php
@@ -1,2 +1,27 @@
 <h2>Üdvözöllek a Receptgyűjteményben!</h2>
-<p>Ez itt a főoldal tartalma. Böngéssz a receptek között!</p>
+<p>Fedezd fel a legfinomabb recepteket, oszd meg saját kedvenceidet és inspirálódj a konyhában!</p>
+<p>Oldalunkon böngészhetsz a különféle kategóriák között, elmentheted a neked tetsző leírásokat, és ha regisztrálsz, akár saját receptet is feltölthetsz képekkel együtt.</p>
+
+<hr> <h3>Egy kis ízelítő</h3>
+
+<div style="margin-bottom: 20px;"> <h4>Rövid bemutató</h4>
+    <video width="400" controls>
+        <source src="videos/fozes_rovid.mp4" type="video/mp4">
+        A böngésződ nem támogatja a videó lejátszását.
+    </video>
+    
+</div>
+
+<div style="margin-bottom: 20px;">
+    <h4>Inspiráció a YouTube-ról</h4>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/FJC1eBRJ-DY?si=Ls4ynGelwX7pXKk8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
+</div>
+
+<hr> <h3>Hol találsz minket?</h3>
+<p>Látogass el hozzánk (vagy a kedvenc alapanyag boltodba):</p>
+
+<div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2728.348606936175!2d21.09081911589469!3d46.6796145791339!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47447e1a633c3b5d%3A0x8085a5e8f8789946!2zQsOp≣scOpY3NhYmEsIEFuZHLDoXNzeSBcdSAxMCwgNTYwMA!5e0!3m2!1shu!2shu!4v1678886450123!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
+</div>
